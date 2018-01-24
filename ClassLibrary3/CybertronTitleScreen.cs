3ab9b60af3a8ad639c4c981f93100b1c10ddf741
Jonathan Markland <jonathan.markland@btinternet.com>
2018-01-24 22:06:07 +0000

    Level 3

--- ClassLibrary3/CybertronTitleScreen.cs
+++ ClassLibrary3/CybertronTitleScreen.cs
@@ -98,7 +98,7 @@ namespace GameClassLibrary
                     TheWorldWallData = loadedWorld,
                     BoardWidth = CybertronGameBoardConstants.ScreenWidth,
                     BoardHeight = CybertronGameBoardConstants.ScreenHeight,
-                    LevelNumber = 1,
+                    LevelNumber = 3,
                     Lives = Constants.InitialLives
                 };
 
