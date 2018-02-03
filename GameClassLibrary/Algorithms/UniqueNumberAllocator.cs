a7b830e75b6505a132d45c267d01bd6433f007e8
Jonathan Markland <jonathan.markland@btinternet.com>
2018-02-03 13:31:51 +0000

    Move around

--- GameClassLibrary/Algorithms/UniqueNumberAllocator.cs
+++ GameClassLibrary/Algorithms/UniqueNumberAllocator.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using GameClassLibrary.Math;
-using GameClassLibrary.Algorithms;
 
-namespace MissionIIClassLibrary
+namespace GameClassLibrary.Algorithms
 {
     public class UniqueNumberAllocator
     {
