9eccac41e28ca157baf1bd19b8dde500562ea3b9
Jonathan Markland <jonathan.markland@btinternet.com>
2018-09-06 18:56:05 +0100

    Comments

--- GameClassLibrary/Walls/Tile.cs
+++ GameClassLibrary/Walls/Tile.cs
@@ -1,8 +1,15 @@
 ﻿namespace GameClassLibrary.Walls
 {
+    /// <summary>
+    /// Represents a kind of tile, which is a user-defined concept.
+    /// </summary>
     public struct Tile
     {
+        /// <summary>
+        /// The index into a user-supplied array of tiles.
+        /// </summary>
         public byte VisualIndex;
+
         public static bool operator==(Tile a, Tile b) { return a.VisualIndex == b.VisualIndex; }
         public static bool operator!=(Tile a, Tile b) { return a.VisualIndex != b.VisualIndex; }
     }
