0b9945feed4fccd2946891cc8805c4277aacb2a1
Jonathan Markland <jonathan.markland@btinternet.com>
2018-09-07 23:39:44 +0100

    Tile enumerations as binary flags

--- MissionIIClassLibrary/MissionIITile.cs
+++ MissionIIClassLibrary/MissionIITile.cs
@@ -4,8 +4,8 @@ namespace MissionIIClassLibrary
 {
     public static class MissionIITile
     {
-        public static Tile Space = new Tile { VisualIndex = 0 };
-        public static Tile Brick = new Tile { VisualIndex = 2 };
-        public static Tile Electric = new Tile { VisualIndex = 4 };
+        public static Tile Space = new Tile { VisualIndex = 2 };
+        public static Tile Brick = new Tile { VisualIndex = 4 };
+        public static Tile Electric = new Tile { VisualIndex = 8 };
     }
 }
