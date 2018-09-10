9802bdbaab0b36b1923093c1f832668743ae576f
Jonathan Markland <jonathan.markland@btinternet.com>
2018-09-10 19:08:18 +0100

    Move PAUSE mode handling in the library.

--- MissionIIClassLibrary/Modes/Pause.cs
+++ MissionIIClassLibrary/Modes/Pause.cs
@@ -1,81 +1,22 @@
 ﻿
-using GameClassLibrary.Graphics;
-using GameClassLibrary.Input;
-using GameClassLibrary.Controls;
 using GameClassLibrary.Modes;
 
 namespace MissionIIClassLibrary.Modes
 {
-    public class Pause : GameMode
+    public class Pause : PauseWithChangeLevel
     {
-        private GameMode _originalMode;
-        private bool _keyReleaseSeen;
-        private bool _restartGameOnNextRelease;
-        private AccessCodeAccumulatorControl _accessCodeControl;
-        private MissionIIGameBoard _theGameBoard;
-
         public Pause(GameMode originalMode, MissionIIGameBoard theGameBoard)
+            : base(
+                  originalMode,
+                  Constants.ScreenHeight - 60,
+                  MissionIISprites.Paused,
+                  MissionIISounds.PauseMode,
+                  MissionIISounds.ManFiring,
+                  MissionIIFonts.GiantFont,
+                  s => theGameBoard.LevelCodeAccepted(s),
+                  () => new Modes.EnteringLevel(theGameBoard),
+                  () => !theGameBoard.Man.IsDead && !theGameBoard.Man.IsBeingElectrocuted)
         {
-            _theGameBoard = theGameBoard;
-            _originalMode = originalMode;
-            _keyReleaseSeen = false; // PAUSE key is held at the time this object is created.
-            _restartGameOnNextRelease = false;
-            _accessCodeControl = new AccessCodeAccumulatorControl(
-                Constants.ScreenWidth / 2,
-                Constants.ScreenHeight - 60,
-                4,
-                OnAccessCodeEntered,
-                MissionIISounds.ManFiring.Play,
-                MissionIIClassLibrary.MissionIIFonts.GiantFont);
-        }
-
-        private void OnAccessCodeEntered(string accessCode)
-        {
-            if (!_theGameBoard.LevelCodeAccepted(accessCode))
-            {
-                _accessCodeControl.ClearEntry();
-                MissionIISounds.PauseMode.Play();
-            }
-            else
-            {
-                ActiveMode = new Modes.EnteringLevel(_theGameBoard);
-            }
-        }
-
-        public override void AdvanceOneCycle(KeyStates theKeyStates)
-        {
-            if (!_keyReleaseSeen)
-            {
-                if (!theKeyStates.Pause)
-                {
-                    _keyReleaseSeen = true;
-                    if (_restartGameOnNextRelease)
-                    {
-                        ExitToOriginalMode();
-                    }
-                }
-            }
-            else if (theKeyStates.Pause)
-            {
-                _restartGameOnNextRelease = true;
-                _keyReleaseSeen = false;
-            }
-            else if (!_theGameBoard.Man.IsDead && !_theGameBoard.Man.IsBeingElectrocuted) // anti-cheating!
-            {
-                _accessCodeControl.AdvanceOneCycle(theKeyStates);
-            }
-        }
-
-        public override void Draw(IDrawingTarget drawingTarget)
-        {
-            _originalMode.Draw(drawingTarget);
-            drawingTarget.DrawFirstSpriteScreenCentred(MissionIISprites.Paused);
-            _accessCodeControl.Draw(drawingTarget);
-        }
-
-        private void ExitToOriginalMode()
-        {
-            ActiveMode = _originalMode;
         }
     }
 }
